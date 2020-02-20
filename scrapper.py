a9c96386a371db8b4f6e4252b778075b2376a9fa
Siddarth Patil <siddarth3639@gmail.com>
2020-02-20 11:02:06 +0530

    Final script
    Completed the script which will check for the price and send a email if condition is met

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,7 @@
 # Helps us in getting the data from the URL
 import requests
 from bs4 import BeautifulSoup
+import smtplib
 
 # URL of the product page which you want.
 URL = "https://www.amazon.in/Bangalore-Refinery-999-9-Yellow-Gold/dp/B01HVB3PPC/ref=sxin_4?ascsubtag=amzn1.osa.841988af-23c9-4aa4-8bf3-522fe12d3817.A21TJRUUN4KGV.en_IN&creativeASIN=B01HVB3PPC&cv_ct_cx=gold&cv_ct_id=amzn1.osa.841988af-23c9-4aa4-8bf3-522fe12d3817.A21TJRUUN4KGV.en_IN&cv_ct_pg=search&cv_ct_wn=osp-single-source&keywords=gold&linkCode=oas&pd_rd_i=B01HVB3PPC&pd_rd_r=b5b3bfc8-1bb1-4041-86f0-4683928aa0b2&pd_rd_w=Uwu8Q&pd_rd_wg=dE82S&pf_rd_p=c08fa7ba-5992-402a-8cce-56eabbfee1ac&pf_rd_r=YC4SKYC5NGAMAMVQX3V3&qid=1582088333&tag=htsyndicate-21#customerReviews"
@@ -20,3 +21,25 @@ def check_price():
     if ( float_price < 22000.0):
         send_mail()
 
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    server.login('<Your email ID>', '<Your 2 way verification app Password>')
+
+    msg = """Subject: Price down alert!
+
+    Check this link out --> https://www.amazon.in/Bangalore-Refinery-999-9-Yellow-Gold/dp/B01HVB3PPC/ref=sxin_4?ascsubtag=amzn1.osa.841988af-23c9-4aa4-8bf3-522fe12d3817.A21TJRUUN4KGV.en_IN&creativeASIN=B01HVB3PPC&cv_ct_cx=gold&cv_ct_id=amzn1.osa.841988af-23c9-4aa4-8bf3-522fe12d3817.A21TJRUUN4KGV.en_IN&cv_ct_pg=search&cv_ct_wn=osp-single-source&keywords=gold&linkCode=oas&pd_rd_i=B01HVB3PPC&pd_rd_r=b5b3bfc8-1bb1-4041-86f0-4683928aa0b2&pd_rd_w=Uwu8Q&pd_rd_wg=dE82S&pf_rd_p=c08fa7ba-5992-402a-8cce-56eabbfee1ac&pf_rd_r=YC4SKYC5NGAMAMVQX3V3&qid=1582088333&tag=htsyndicate-21#customerReview"""
+
+    server.sendmail(
+        '<From eMail ID>',
+        '<To eMail IDs>',
+        msg
+    )
+
+    print("Email sent.")
+    server.quit()
+
+check_price()
